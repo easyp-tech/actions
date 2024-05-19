2c6ab8a13ec0f5cfb0064ee17696bcaf53376f53
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:05:42 +0300

    bug: fix action version

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -14,30 +14,19 @@ fi
 
 # Проверка, что указан каталог
 if [ -z "$2" ]; then
-  echo "::error::Please provide the directory path to lint."
+  echo "::error ::Please provide the directory path to lint."
   exit 1
 fi
 
-# Запуск линтера и обработка вывода
-lint_output=$(easyp lint -p $2)
-
-if [ $? -eq 0 ]; then
-  echo "Linting passed with no errors."
-else
-  echo "Linting found issues."
-  echo "$lint_output"
-
-  # Пример обработки вывода линтера для предоставленного формата:
-  echo "$lint_output" | while read -r line; do
-    if echo "$line" | grep -qE '(.+):([0-9]+):([0-9]+):(.+): (.+)'; then
-      filename=$(echo "$line" | cut -d ':' -f 1)
-      linenumber=$(echo "$line" | cut -d ':' -f 2)
-      columnnumber=$(echo "$line" | cut -d ':' -f 3)
-      code=$(echo "$line" | cut -d ':' -f 4)
-      message=$(echo "$line" | cut -d ':' -f 5-)
-      echo "::error file=$filename,line=$linenumber,col=$columnnumber::$code - $message"
+# Запуск линтера и печать результатов в формате GitHub Annotations
+easyp lint -p $2 | while read -r line; do
+    if echo "$line" | grep -qE '([^:]+):([0-9]+):([0-9]+):([^:]+):(.+)'; then
+      FILE=$(echo "$line" | cut -d ':' -f 1)
+      LINE=$(echo "$line" | cut -d ':' -f 2)
+      COL=$(echo "$line" | cut -d ':' -f 3)
+      # CODE is not used directly but might be handy for including in the message
+      CODE=$(echo "$line" | cut -d ':' -f 4)
+      MESSAGE=$(echo "$line" | cut -d ':' -f 5-)
+      echo "::error file=$FILE,line=$LINE,col=$COL::$MESSAGE"
     fi
-  done
-
-  exit 1
-fi
+done
