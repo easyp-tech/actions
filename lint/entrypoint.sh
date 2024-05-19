a912f9979ae6c80d0b23e5e3395e36fccd01cb0b
Edgar Sipki <edo7796@gmail.com>
2024-05-19 14:08:07 +0300

    bug: fix action version

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -1,4 +1,16 @@
 #!/bin/sh -l
 
+# Check EasyP is installed
+echo ls -l /usr/local/go/bin
+
+# Check if the version is provided
+if [ -z "$1" ]
+then
+  echo "Please provide the directory path to lint."
+  exit 1
+fi
+
+
+
 # Run linter
 easyp lint -p $1
