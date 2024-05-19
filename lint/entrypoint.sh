696fff8651a09186586698520cbabd3fd5ba6d0b
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:02:01 +0300

    bug: fix action version

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -8,15 +8,36 @@ go install github.com/easyp-tech/easyp/cmd/easyp@$1
 
 # Проверка, что EasyP установлен
 if ! command -v easyp > /dev/null; then
-  echo "EasyP installation failed or EasyP is not in the PATH"
+  echo "::error::EasyP installation failed or EasyP is not in the PATH"
   exit 1
 fi
 
 # Проверка, что указан каталог
 if [ -z "$2" ]; then
-  echo "Please provide the directory path to lint."
+  echo "::error::Please provide the directory path to lint."
   exit 1
 fi
 
-# Запуск линтера
-easyp lint -p $2
+# Запуск линтера и обработка вывода
+lint_output=$(easyp lint -p $2)
+
+if [ $? -eq 0 ]; then
+  echo "Linting passed with no errors."
+else
+  echo "Linting found issues."
+  echo "$lint_output"
+
+  # Пример обработки вывода линтера для предоставленного формата:
+  echo "$lint_output" | while read -r line; do
+    if echo "$line" | grep -qE '(.+):([0-9]+):([0-9]+):(.+): (.+)'; then
+      filename=$(echo "$line" | cut -d ':' -f 1)
+      linenumber=$(echo "$line" | cut -d ':' -f 2)
+      columnnumber=$(echo "$line" | cut -d ':' -f 3)
+      code=$(echo "$line" | cut -d ':' -f 4)
+      message=$(echo "$line" | cut -d ':' -f 5-)
+      echo "::error file=$filename,line=$linenumber,col=$columnnumber::$code - $message"
+    fi
+  done
+
+  exit 1
+fi
