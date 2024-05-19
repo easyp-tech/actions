fed9bb276885133c607b0e8e4bec652635bf833d
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:57:23 +0300

    bug: fix action script

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -7,7 +7,7 @@ export PATH=$PATH:/go/bin
 go install github.com/easyp-tech/easyp/cmd/easyp@$1
 
 # Проверка, что EasyP установлен
-if ! command -v easyp > /dev/null; then
+if! command -v easyp > /dev/null; then
   echo "EasyP installation failed or EasyP is not in the PATH"
   exit 1
 fi
@@ -18,26 +18,7 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-# Запуск линтера и запись вывода в файл
-OUTPUT_FILE="./easyp_lint_output.txt"
-easyp lint -p "$2" > "$OUTPUT_FILE"
-
-# Проверяем, обнаружены ли ошибки
-if grep -q ":" "$OUTPUT_FILE"; then
-  # Ошибки найдены, создаем аннотации для GitHub Actions
-  while IFS= read -r line; do
-    if echo "$line" | grep -q ":"; then
-      FILE_PATH=$(echo "$line" | awk -F ':' '{print $1}')
-      LINE=$(echo "$line" | awk -F ':' '{print $2}')
-      COLUMN=$(echo "$line" | awk -F ':' '{print $3}')
-      MESSAGE=$(echo "$line" | awk -F ':' '{print $4}')
-      # Создаем аннотацию ошибки для GitHub Actions
-      echo "::error file=$FILE_PATH,line=$LINE,col=$COLUMN::$MESSAGE"
-    fi
-  done < "$OUTPUT_FILE"
-  exit 1
-else
-  echo "No linting errors found."
-fi
-
-exit 0
+# Запуск линтера
+easyp lint -p $2 | while IFS=: read -r filepath line column code message; do
+  echo "::error file=$filepath,line=$line,column=$column::$message"
+done
