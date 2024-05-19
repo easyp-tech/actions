26f3bad0c1b1a1a1693d06066e4ef111d712e01a
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:19:38 +0300

    bug: fix action script

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -8,25 +8,27 @@ go install github.com/easyp-tech/easyp/cmd/easyp@$1
 
 # Проверка, что EasyP установлен
 if ! command -v easyp > /dev/null; then
-  echo "::error::EasyP installation failed or EasyP is not in the PATH"
+  echo "EasyP installation failed or EasyP is not in the PATH"
   exit 1
 fi
 
 # Проверка, что указан каталог
 if [ -z "$2" ]; then
-  echo "::error ::Please provide the directory path to lint."
+  echo "Please provide the directory path to lint."
   exit 1
 fi
 
-# Запуск линтера и печать результатов в формате GitHub Annotations
-easyp lint -p $2 | while read -r line; do
-    if echo "$line" | grep -qE '([^:]+):([0-9]+):([0-9]+):([^:]+):(.+)'; then
-      FILE=$(echo "$line" | cut -d ':' -f 1)
-      LINE=$(echo "$line" | cut -d ':' -f 2)
-      COL=$(echo "$line" | cut -d ':' -f 3)
-      # CODE is not used directly but might be handy for including in the message
-      CODE=$(echo "$line" | cut -d ':' -f 4)
-      MESSAGE=$(echo "$line" | cut -d ':' -f 5-)
-      echo "::error file=$FILE,line=$LINE,col=$COL::$MESSAGE"
-    fi
+# Запуск линтера и вывод ошибок в формате, понятном GitHub Actions
+easyp lint -p $2 | while IFS= read -r line; do
+  # Предполагаем, что формат строки ошибки: filepath:line:col:CODE: message
+  FILE_PATH=$(echo "$line" | cut -d: -f1)
+  LINE=$(echo "$line" | cut -d: -f2)
+  COLUMN=$(echo "$line" | cut -d: -f3)
+  MESSAGE=$(echo "$line" | cut -d: -f4-)
+  echo "::error file=$FILE_PATH,line=$LINE,col=$COLUMN::$MESSAGE"
 done
+
+# Если вы хотите завершить работу с ненулевым статусом в случае обнаружения ошибок
+if [ -s easyp_lint_output.txt ]; then
+  exit 1
+fi
