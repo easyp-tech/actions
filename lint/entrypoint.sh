7736b286090963d138dce054c54aa12a3d105d00
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:39:52 +0300

    bug: fix action script

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -18,32 +18,23 @@ if [ -z "$2" ]; then
   exit 1
 fi
 
-# Запуск линтера и сохранение вывода в переменную
-OUTPUT=$(easyp lint -p $2)
-echo "$OUTPUT"
+# Запуск линтера и запись вывода в файл
+OUTPUT_FILE=easyp_lint_output.txt
+easyp lint -p "$2" > "$OUTPUT_FILE"
 
-# Переменная для отслеживания наличия ошибок
-ERROR_DETECTED=0
-
-# Анализируем каждую строку вывода
-echo "$OUTPUT" | while read -r line; do
-  # Проверяем, содержит ли строка путь к файлу и номер строки, что указывает на ошибку
-  if echo "$line" | grep -q ":[0-9]\+:[0-9]\+:"; then
-    ERROR_DETECTED=1
-    # Извлекаем данные из строки
+# Проверяем, обнаружены ли ошибки
+if grep -q ":" "$OUTPUT_FILE"; then
+  # Ошибки найдены, печатаем их и создаем аннотации для GitHub Actions
+  while IFS= read -r line; do
     FILE_PATH=$(echo "$line" | cut -d: -f1)
     LINE=$(echo "$line" | cut -d: -f2)
     COLUMN=$(echo "$line" | cut -d: -f3)
     MESSAGE=$(echo "$line" | cut -d: -f4-)
-    # Создаем аннотацию ошибки для GitHub Actions
     echo "::error file=$FILE_PATH,line=$LINE,col=$COLUMN::$MESSAGE"
-  fi
-done
-
-# Если были обнаружены ошибки, завершаем с ошибкой
-if [ "$ERROR_DETECTED" -eq 1 ]; then
+  done < "$OUTPUT_FILE"
   exit 1
+else
+  echo "No linting errors found."
 fi
 
-# Если ошибок не обнаружено, скрипт завершится успешно
 exit 0
