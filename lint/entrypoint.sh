1db6e113de9a7a15aaf32c0418c9636a59a49e00
Edgar Sipki <edo7796@gmail.com>
2024-05-19 20:59:39 +0300

    bug: fix action script

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -7,18 +7,23 @@ export PATH=$PATH:/go/bin
 go install github.com/easyp-tech/easyp/cmd/easyp@$1
 
 # Проверка, что EasyP установлен
-if! command -v easyp > /dev/null; then
+if! command -v easyp > /dev/null
+then
   echo "EasyP installation failed or EasyP is not in the PATH"
   exit 1
 fi
 
 # Проверка, что указан каталог
-if [ -z "$2" ]; then
+if [ -z "$2" ]
+then
   echo "Please provide the directory path to lint."
   exit 1
 fi
 
 # Запуск линтера
-easyp lint -p $2 | while IFS=: read -r filepath line column code message; do
-  echo "::error file=$filepath,line=$line,column=$column::$message"
+easyp lint -p $2 | while IFS=: read -r filepath line column message; do
+  # Detect errors based on the format of the error message
+  if echo "$message" | grep -qE '^[A-Za-z_][A-Za-z0-9_]*: '; then
+    echo "::error file=$filepath,line=$line,column=$column::$message"
+  fi
 done
