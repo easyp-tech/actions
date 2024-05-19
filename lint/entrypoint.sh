041bf317c6f7fd8e830fa6cbb8fe47b2b72a34a6
Edgar Sipki <edo7796@gmail.com>
2024-05-19 14:29:41 +0300

    bug: fix action version

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh -l
 
 export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/go/bin
 
+# Установка EasyP указанной версии
 go install github.com/easyp-tech/easyp/cmd/easyp@$1
 
-# Check if the version is provided
-if [ -z "$2" ]
-then
+# Проверка, что EasyP установлен
+if ! command -v easyp > /dev/null; then
+  echo "EasyP installation failed or EasyP is not in the PATH"
+  exit 1
+fi
+
+# Проверка, что указан каталог
+if [ -z "$2" ]; then
   echo "Please provide the directory path to lint."
   exit 1
 fi
 
-# Run linter
+# Запуск линтера
 easyp lint -p $2
