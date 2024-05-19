7914e09c5bde9a2f496693de874b787d2076b536
Edgar Sipki <edo7796@gmail.com>
2024-05-19 21:17:39 +0300

    bug: fix action script

--- lint/entrypoint.sh
+++ lint/entrypoint.sh
@@ -25,12 +25,20 @@ fi
 output=$(easyp lint -p "$2")
 
 # Emit GitHub Actions error logs
-while IFS= read -r line; do
-  if [[ "$line" =~ ^([^[:space:]]+)[[:space:]]*:([[:digit:]]+):([[:digit:]]+).* ]]; then
-    file=${BASH_REMATCH[1]}
-    line=${BASH_REMATCH[2]}
-    col=${BASH_REMATCH[3]}
-    issue=$(printf "%s:%s:%s Missing semicolon" "$file" "$line" "$col")
-    echo "::error file=$file,line=$line,col=$col::$issue"
+
+# for format 'echo "::error file=app.js,line=1,col=5::Missing semicolon"'
+# example currency/v1/currency.proto:321:3:CODE_ZWR: enum value comment is empty
+# will be converted to
+# echo "::error file=currency/v1/currency.proto,line=321,col=3::CODE_ZWR: enum value comment is empty"
+# must ignore not matching lines
+
+# Process each line of the output
+echo "$output" | while IFS= read -r line; do
+  if [[ $line =~ ^(.+):([0-9]+):([0-9]+):(.+)$ ]]; then
+    # If the line matches the format file:line:column:message, format it for GitHub Actions
+    echo "::error file=${BASH_REMATCH[1]},line=${BASH_REMATCH[2]},col=${BASH_REMATCH[3]}::${BASH_REMATCH[4]}"
+  else
+    # If the line does not match the expected format, print it as is
+    echo "$line"
   fi
-done <<< "$output"
+done
